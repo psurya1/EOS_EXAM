d8264683cf1d81cda1751874cab2bbc9e1184278
psurya1 <suryaswaminathan1999@gmail.com>
2021-12-14 12:48:55 +0530

    question 1

--- QUESTION_1/p2.c
+++ QUESTION_1/p2.c
@@ -6,11 +6,13 @@
 #include <sys/stat.h>    
 #include <mqueue.h>
 #include <mqueue.h>
+#include <string.h>
 
 mqd_t cdacmq;
 struct mq_attr d2;
 unsigned char buff[64];
 int msg_prio;
+int c;
 
 
 int main(int argc,char const *argv[])
@@ -23,7 +25,9 @@ int main(int argc,char const *argv[])
     cdacmq=mq_open("/dictionary.txt", O_RDONLY | O_CREAT ,S_IRUSR | S_IWUSR,&d2);
         
     mq_receive(cdacmq,buff,64,&msg_prio);
+    c=strlen(buff);
     printf("RECEIVED MESSAGE :%s PRIORITY :%d",buff,msg_prio);
+    printf("LENGTH:%d\n",c);
     mq_close(cdacmq);
     return 0;
 }
